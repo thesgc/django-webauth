64f555d9ba4bf18019e76a2bf45527907922a585
Andrew Stretton <astretton@localhost.localdomain>
2014-06-04 14:43:57 +0100

    removing the cud database

--- django_webauth/backends/webauth_cud.py
+++ django_webauth/backends/webauth_cud.py
@@ -29,21 +29,21 @@ class WebauthCUDBackend(object):
                 'format': 'json',
                 'history': 'n',
                 }
-        cud_data = requests.get(self.cud_endpoint, params=query, auth=HTTPKerberosAuth()).json
-        assert len(cud_data['cudSubjects']) == 1
-        subject = cud_data['cudSubjects'][0]
-        attributes = {}
-        for attr in subject['attributes']:
-            attributes[attr['name']] = attr['value']
-        user.first_name = attributes['cud:cas:firstname']
-        user.last_name = attributes['cud:cas:lastname']
-        user.email = attributes['cud:cas:oxford_email']
-        for name in attributes['cud:cas:current_affiliation']:
-            group, created = Group.objects.get_or_create(name=name)
-            user.groups.add(group)
-        user.save()
+        # cud_data = requests.get(self.cud_endpoint, params=query, auth=HTTPKerberosAuth()).json
+        # assert len(cud_data['cudSubjects']) == 1
+        # subject = cud_data['cudSubjects'][0]
+        # attributes = {}
+        # for attr in subject['attributes']:
+        #     attributes[attr['name']] = attr['value']
+        # user.first_name = attributes['cud:cas:firstname']
+        # user.last_name = attributes['cud:cas:lastname']
+        # user.email = attributes['cud:cas:oxford_email']
+        # for name in attributes['cud:cas:current_affiliation']:
+        #     group, created = Group.objects.get_or_create(name=name)
+        #     user.groups.add(group)
+        # user.save()
         user.backend = 'django_webauth.backends.webauth_cud.WebauthCUDBackend'
-        user.cud_attributes = attributes
+        #user.cud_attributes = attributes
         return user
 
     def get_user(self, user_id):
