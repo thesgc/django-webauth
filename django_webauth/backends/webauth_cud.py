0c7c4fbb8ade953a176c4e3e535c758492522ac5
Andrew Stretton <astretton@localhost.localdomain>
2014-06-05 11:48:23 +0100

    editing the LDAP config to allow for custom users and adding the new set_unusable_password method call to replace the deprecated unusable password also added exception handling to cud backend but this will likely fail

--- django_webauth/backends/webauth_cud.py
+++ django_webauth/backends/webauth_cud.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.contrib.auth.models import  Group
 from requests_kerberos import HTTPKerberosAuth
 from django.contrib.auth import get_user_model
-import random
 
 User = get_user_model()
 
@@ -19,8 +18,7 @@ class WebauthCUDBackend(object):
 
     def authenticate(self, username):
 
-        hash = random.getrandbits(128)
-        user, created = User.objects.get_or_create(username=username, defaults={'password': "%032x" % hash})
+        user, created = User.objects.get_or_create(username=username )
         if created:
             logger.info("Created user: (%s - id:%s)" % (username, user.id))
         else:
@@ -29,22 +27,28 @@ class WebauthCUDBackend(object):
                 'format': 'json',
                 'history': 'n',
                 }
-        # cud_data = requests.get(self.cud_endpoint, params=query, auth=HTTPKerberosAuth()).json
-        # assert len(cud_data['cudSubjects']) == 1
-        # subject = cud_data['cudSubjects'][0]
-        # attributes = {}
-        # for attr in subject['attributes']:
-        #     attributes[attr['name']] = attr['value']
-        # user.first_name = attributes['cud:cas:firstname']
-        # user.last_name = attributes['cud:cas:lastname']
-        # user.email = attributes['cud:cas:oxford_email']
-        # for name in attributes['cud:cas:current_affiliation']:
-        #     group, created = Group.objects.get_or_create(name=name)
-        #     user.groups.add(group)
-        # user.save()
-        user.backend = 'django_webauth.backends.webauth_cud.WebauthCUDBackend'
-        #user.cud_attributes = attributes
-        return user
+        user.set_unusable_password()
+        try:
+            cud_data = requests.get(self.cud_endpoint, params=query, auth=HTTPKerberosAuth()).json
+            assert len(cud_data['cudSubjects']) == 1
+            subject = cud_data['cudSubjects'][0]
+            attributes = {}
+            for attr in subject['attributes']:
+                attributes[attr['name']] = attr['value']
+            user.first_name = attributes['cud:cas:firstname']
+            user.last_name = attributes['cud:cas:lastname']
+            user.email = attributes['cud:cas:oxford_email']
+            for name in attributes['cud:cas:current_affiliation']:
+                group, created = Group.objects.get_or_create(name=name)
+                user.groups.add(group)
+            user.save()
+            user.backend = 'django_webauth.backends.webauth_cud.WebauthCUDBackend'
+            user.cud_attributes = attributes
+            return user
+        except Exception:
+            raise ImproperlyConfigured('Issue with webauth cud, have you configured your kerberos keytab for cud access?')
+        return None
+
 
     def get_user(self, user_id):
         try:
